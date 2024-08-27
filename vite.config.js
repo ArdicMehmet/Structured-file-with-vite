2d65067105d780f1a08c66afb3aceec2c27837e2
Mehmet Ardic <ardicmehmet96@gmail.com>
2024-08-27 20:56:10 +0300

    changed bg

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/Structured-file-with-vite/', // Uygulamanızın barındırılacağı alt dizin
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
